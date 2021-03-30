69b68fccfd147e25100f237abccd33a6ece7b309
Fangqiao Hu <20763670+fangqiaohu@users.noreply.github.com>
2021-03-30 18:46:22 +0800

    Update unet_parts.py

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -5,81 +5,38 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-# class _SelfAdaption(nn.Module):
-#     """self-adaption, e.g: bs*3*28*28 -> bs*(3*4)*28*28 -> bs*(3*4)*28*28 -> bs*3*28*28, kernel_size=1"""
-#     # https://discuss.pytorch.org/t/how-to-modify-a-conv2d-to-depthwise-separable-convolution/15843
-#
-#     def __init__(self, in_channels):
-#         super(_SelfAdaption, self).__init__()
-#         self.in_channels = in_channels
-#         self.kernel_size = 1
-#         self.padding = 0
-#         self.stride = 1
-#         self.hidden_channels = 4
-#
-#         self.add_module('relu1', nn.ReLU())
-#         self.add_module('convs1', nn.Conv2d(self.in_channels, self.hidden_channels * self.in_channels,
-#                                             kernel_size=self.kernel_size, groups=self.in_channels,
-#                                             stride=self.stride, padding=self.padding))
-#
-#         # self.add_module('relu2', nn.ReLU())
-#         # self.add_module('convs2', nn.Conv2d(self.hidden_channels * self.in_channels, self.hidden_channels * self.in_channels,
-#         #                                     kernel_size=self.kernel_size, groups=self.in_channels,
-#         #                                     stride=self.stride, padding=self.padding))
-#
-#         self.add_module('relu3', nn.ReLU())
-#         self.add_module('convs3', nn.Conv2d(self.hidden_channels * self.in_channels, self.in_channels,
-#                                             kernel_size=self.kernel_size, groups=self.in_channels,
-#                                             stride=self.stride, padding=self.padding))
-#
-#     def forward(self, x):
-#         x = self.convs1(self.relu1(x))
-#         # x = self.convs2(self.relu2(x))
-#         x = self.convs3(self.relu3(x))
-#         return x
-#
-#
-# class DoubleConv(nn.Module):
-#     """(convolution => [BN] => ReLU) * 2"""
-#
-#     def __init__(self, in_channels, out_channels, mid_channels=None):
-#         super().__init__()
-#         if not mid_channels:
-#             mid_channels = out_channels
-#
-#         self.lambda1 = 1
-#
-#         # DEBUG: adding SASA
-#         self.adp = _SelfAdaption(in_channels=out_channels)
-#
-#         self.double_conv = nn.Sequential(
-#             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
-#             nn.BatchNorm2d(mid_channels),
-#             nn.ReLU(inplace=True),
-#             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
-#             nn.BatchNorm2d(out_channels),
-#             nn.ReLU(inplace=True)
-#         )
-#
-#         # self.double_conv1 = nn.Sequential(
-#         #     nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
-#         #     nn.BatchNorm2d(mid_channels),
-#         #     nn.ReLU(inplace=True)
-#         # )
-#         #
-#         # self.double_conv2 = nn.Sequential(
-#         #     nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
-#         #     nn.BatchNorm2d(out_channels),
-#         #     nn.ReLU(inplace=True)
-#         # )
-#
-#     def forward(self, x):
-#         # x = self.double_conv1(x)
-#         # # x = x + self.lambda1 * self.adp1(x)
-#         # x = self.double_conv2(x)
-#         x = self.double_conv(x)
-#         y = self.lambda1 * self.adp(x)
-#         return x + y
+class _SelfAdaption(nn.Module):
+    """self-adaption, e.g: bs*3*28*28 -> bs*(3*4)*28*28 -> bs*(3*4)*28*28 -> bs*3*28*28, kernel_size=1"""
+    # https://discuss.pytorch.org/t/how-to-modify-a-conv2d-to-depthwise-separable-convolution/15843
+
+    def __init__(self, in_channels):
+        super(_SelfAdaption, self).__init__()
+        self.in_channels = in_channels
+        self.kernel_size = 1
+        self.padding = 0
+        self.stride = 1
+        self.hidden_channels = 4
+
+        self.add_module('relu1', nn.ReLU())
+        self.add_module('convs1', nn.Conv2d(self.in_channels, self.hidden_channels * self.in_channels,
+                                            kernel_size=self.kernel_size, groups=self.in_channels,
+                                            stride=self.stride, padding=self.padding))
+
+        # self.add_module('relu2', nn.ReLU())
+        # self.add_module('convs2', nn.Conv2d(self.hidden_channels * self.in_channels, self.hidden_channels * self.in_channels,
+        #                                     kernel_size=self.kernel_size, groups=self.in_channels,
+        #                                     stride=self.stride, padding=self.padding))
+
+        self.add_module('relu3', nn.ReLU())
+        self.add_module('convs3', nn.Conv2d(self.hidden_channels * self.in_channels, self.in_channels,
+                                            kernel_size=self.kernel_size, groups=self.in_channels,
+                                            stride=self.stride, padding=self.padding))
+
+    def forward(self, x):
+        x = self.convs1(self.relu1(x))
+        # x = self.convs2(self.relu2(x))
+        x = self.convs3(self.relu3(x))
+        return x
 
 
 class DoubleConv(nn.Module):
@@ -90,6 +47,11 @@ class DoubleConv(nn.Module):
         if not mid_channels:
             mid_channels = out_channels
 
+        self.lambda1 = 1
+
+        # DEBUG: adding SASA
+        self.adp = _SelfAdaption(in_channels=out_channels)
+
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(mid_channels),
@@ -99,8 +61,46 @@ class DoubleConv(nn.Module):
             nn.ReLU(inplace=True)
         )
 
+        # self.double_conv1 = nn.Sequential(
+        #     nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
+        #     nn.BatchNorm2d(mid_channels),
+        #     nn.ReLU(inplace=True)
+        # )
+        #
+        # self.double_conv2 = nn.Sequential(
+        #     nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
+        #     nn.BatchNorm2d(out_channels),
+        #     nn.ReLU(inplace=True)
+        # )
+
     def forward(self, x):
-        return self.double_conv(x)
+        # x = self.double_conv1(x)
+        # # x = x + self.lambda1 * self.adp1(x)
+        # x = self.double_conv2(x)
+        x = self.double_conv(x)
+        y = self.lambda1 * self.adp(x)
+        return x + y
+
+
+# class DoubleConv(nn.Module):
+#     """(convolution => [BN] => ReLU) * 2"""
+
+#     def __init__(self, in_channels, out_channels, mid_channels=None):
+#         super().__init__()
+#         if not mid_channels:
+#             mid_channels = out_channels
+
+#         self.double_conv = nn.Sequential(
+#             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
+#             nn.BatchNorm2d(mid_channels),
+#             nn.ReLU(inplace=True),
+#             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
+#             nn.BatchNorm2d(out_channels),
+#             nn.ReLU(inplace=True)
+#         )
+
+#     def forward(self, x):
+#         return self.double_conv(x)
 
 
 class Down(nn.Module):
